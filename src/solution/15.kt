687b0c66d6353dd2d9238e160cd2c99e7569f9e1
Martin de Laat <martin.delaat@nedap.com>
2025-02-06 11:59:34 +0100

    set up of 15

---
+++ src/solution/15.kt
@@ -0,0 +1,29 @@
+package solution
+
+import println
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+fun main() {
+    Day15(Path("src/input/day15/input.txt").readText()).run().println()
+}
+
+class Day15(input: String) {
+    private val str = input
+    private val steps = str.filterNot { it.isWhitespace() }.split(',')
+
+    fun run(): Int {
+        return steps.map(::runHash).sum()
+    }
+
+    private fun runHash(str: String): Int {
+        var curVal = 0
+        str.forEach { char ->
+            curVal += char.hashCode()
+            curVal *= 17
+            curVal %= 256
+        }
+        println("$str becomes $curVal")
+        return curVal
+    }
+}
